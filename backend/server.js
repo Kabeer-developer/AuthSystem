09964e3ae81af2e6bfdd81a592ef4c90f653d544
kabeer <kabeeratwork7@gmail.com>
2026-01-22 15:20:28 +0530

    tested and verified

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDb from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
@@ -8,6 +9,7 @@ dotenv.config();
 connectDb();
 
 const app = express();
+app.use(cors()); 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
